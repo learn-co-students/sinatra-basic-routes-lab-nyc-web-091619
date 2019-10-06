8e5fcfedf9974ada94bc5ec6b180ab4d676fa6e3
Altaf Quadri <altafmquadri@gmail.com>
2019-10-06 17:42:25 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,9 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get ('/name') {"My name is Altaf"}
+    get ('/hometown') {"My hometown is Queens"}
+    get ('/favorite-song') {"My favorite song is Thodi Der"}
+    
 end
