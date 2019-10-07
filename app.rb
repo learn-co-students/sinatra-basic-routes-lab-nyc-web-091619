75057abe21346891c49a8854260e03b2a0c24497
Liam <liam.healy1@marist.edu>
2019-10-07 13:45:26 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    resp = Rack::Response.new
+
+    get '/name' do
+        resp.status = 200
+        resp.write "My name is Liam."
+    end
+
+    get '/hometown' do
+        resp.status = 200
+        resp.write "My hometown is _"
+    end
+
+    get '/favorite-song' do
+        resp.status = 200
+        resp.write "My favorite song is _"
+    end
+
+
 end
