9252374fd512bcacd44cbb011ea03e419564ccaf
divbeech <dyadav1006@gmail.com>
2019-10-08 16:16:45 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    resp = Rack::Response.new
+
+    get '/name' do
+        resp.write "My name is Divya Beech."
+    end
+
+    get '/hometown' do
+        resp.write "My hometown is _."
+    end
+
+    get '/favorite-song' do
+        resp.write "My favorite song is _."
+    end
 end
